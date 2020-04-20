b078b1a8b6d3bc7e3241aa45ec2f553c6db68ccc
Invisible-de-v <Invisible-de-v@users.noreply.github.com>
2020-04-20 18:40:49 +0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        int a = 4, b = 3;
+        int c = a + b;
+        Console.WriteLine(c);
+
+    }
+}
+// И куда-то съехала скобочка
+//Добвляем точечки с запятой}
